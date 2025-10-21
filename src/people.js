3157e5b299c9e1e1bb429647da509a01e9550c15
xzy.sung <xzy.sung@gmail.com>
2025-10-21 21:15:38 +0000

    backend-finalizado

---
+++ src/people.js
@@ -0,0 +1,21 @@
+const people = [
+    {
+        name: "Felipe",
+        age: 20,
+        email: "xzy@gmail.com"
+    },
+
+    {
+        name: "Lorenzo",
+        age: 10,
+        email: "zyx@gmail.com"
+    },
+
+    {
+        name: "Chaves",
+        age: 15,
+        email: "yxz@gmail.com"
+    }
+]
+
+export { people }

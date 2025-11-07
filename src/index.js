7fe171c48cece4f444430baa87a2b9458935a4ab
xzy.sung <xzy.sung@gmail.com>
2025-11-07 01:43:43 +0000

    add: idade e para aparecer no terminal

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ const port = 3333
 
 app.use(cors())
 
+app.use(express.json())
+
 app.get("/", (request, response) => {
   response.json (people)
 
+  })
+
+  app.post("/cadastrar", (request, response) => {
+    const {name, age, email, password} = request.body.user
+
+    console.log(`
+      Nome: ${name}, 
+      Idade: ${age},
+      Email: ${email}, 
+      Senha: ${password} 
+      `)
+    response.status(201).json({message: "Usuário cadastrado com sucesso!"})
   })
 
   app.listen(port, () =>{
